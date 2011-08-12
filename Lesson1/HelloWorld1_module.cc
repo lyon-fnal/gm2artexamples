6348889c8c6291bd0e82a0d8c4b6f0e90c17cc5f
Adam Lyon <lyon@fnal.gov>
2011-08-12 17:39:55 -0500

    Stuff for lesson1

--- Lesson1/HelloWorld1_module.cc
+++ Lesson1/HelloWorld1_module.cc
@@ -1,10 +1,11 @@
 //
 //  The HelloWorld1 plugin; the first example of an analyzer module.
+//   You can generate boilerplate with the "artmod" command
 //
 //  $Id: $
 //  $Author: $
 //  $Date: $
-//   
+//
 
 // Includes from art and the packages on which it depends.
 #include "art/Framework/Core/EDAnalyzer.h"
@@ -12,21 +13,27 @@
 
 using namespace std;
 
+// Make a namespace for your stuff (artex = art examples)
 namespace artex {
 
+  // Declare the class for our analyzer
   class HelloWorld1 : public art::EDAnalyzer {
 
   public:
     explicit HelloWorld1(fhicl::ParameterSet const& pset);
     // Compiler generated d'tor is OK.
 
+    // The part that looks at each event
     void analyze(const art::Event& event );
 
   };
 
+  // Definition for constructor
   HelloWorld1::HelloWorld1(fhicl::ParameterSet const& pset){
+    // Don't do anything
   }
 
+  // Definition of analyze
   void HelloWorld1::analyze(const art::Event& event){
     mf::LogVerbatim("test")
       << "Hello, world. From analyze. "
@@ -35,5 +42,6 @@ namespace artex {
 
 } // end namespace artex
 
+// Some red-tape for the Art system
 using artex::HelloWorld1;
 DEFINE_ART_MODULE(HelloWorld1);
